d100bacf35380d5f44c46354b904fabff67c7e94
Tom Utley <tom@tomutley.com>
2017-01-26 23:12:49 -0500

    refactored middleware

--- helpers/startup.go
+++ helpers/startup.go
@@ -6,6 +6,14 @@ import (
 	mgo "gopkg.in/mgo.v2"
 
 	"github.com/tutley/chiact/models"
+	"net/http"
+	"log"
+	"strings"
+
+	"encoding/base64"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
 )
 
 type key int
@@ -33,3 +41,127 @@ func GetUser(ctx context.Context) *users.User {
 func SetJwtSecret(secret []byte) {
 	JwtSecret = secret
 }
+
+
+// MongoMiddleware gives us our connection to the database
+func MongoMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// setup the mgo connection
+		session, err := mgo.Dial("mongodb://localhost/") // TODO: make this a config var
+
+		if err != nil {
+			panic(err)
+		}
+
+		reqSession := session.Clone()
+		defer reqSession.Close()
+		db := reqSession.DB("chiact") // TODO: Make this a config var
+		ctx := context.WithValue(r.Context(), DbKey, db)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+
+// BasicMiddleware lets us process logins
+func BasicMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		db := GetDb(r.Context())
+		if db == nil {
+			log.Print("No database context")
+			http.Error(w, "Not authorized", 401)
+		}
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		if len(s) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		b, err := base64.StdEncoding.DecodeString(s[1])
+		if err != nil {
+			http.Error(w, err.Error(), 401)
+			return
+		}
+
+		pair := strings.SplitN(string(b), ":", 2)
+		if len(pair) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		//Find the user in the database
+		user, err := users.FindUserByEmail(pair[0], db)
+		if err != nil || user == nil {
+			log.Printf("User %+v not found.", pair[0])
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		//Check their password
+		err = user.CheckPassword(pair[1])
+		if err != nil {
+			log.Printf("Invalid password for User: %+v.", pair[0])
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		//clear password
+		user.Password = ""
+
+		//Set the logged in user in the context
+		ctx := context.WithValue(r.Context(), UserKey, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+}
+
+// JwtAuthMiddleware handles the JWT authentication strategy
+func JwtAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		db := GetDb(r.Context())
+		if db == nil {
+			log.Print("No database context")
+			http.Error(w, "Not authorized", 401)
+		}
+
+		token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor, func(token *jwt.Token) (interface{}, error) {
+			return JwtSecret, nil
+		})
+
+		if err != nil {
+			http.Error(w, "Invalid Token", 401)
+			return
+		}
+
+		if !token.Valid {
+			http.Error(w, "Invalid Token", 401)
+			return
+		}
+
+		// Check signing method to avoid vulnerabilities
+		if token.Method != jwt.SigningMethodHS256 {
+			http.Error(w, "Invalid Token", 401)
+			return
+		}
+		claims := token.Claims.(jwt.MapClaims)
+		//Find the user in the database
+		user, err := users.FindUserByID(claims["id"].(string), db)
+		if err != nil || user == nil {
+			log.Printf("User %s not found.", claims["id"].(string))
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		//clear password
+		user.Password = ""
+
+		//Set the logged in user in the context
+		ctx := context.WithValue(r.Context(), UserKey, user)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+
--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-
-	"context"
-	"encoding/base64"
-	"strings"
-
-	mgo "gopkg.in/mgo.v2"
-
-	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/dgrijalva/jwt-go/request"
-	"github.com/tutley/chiact/models"
 
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/middleware"
@@ -29,9 +18,13 @@ func main() {
 
 	// Use Chi built-in middlewares
 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	// Mount database
-	r.Use(ChiMongoMiddleware)
+	r.Use(startup.MongoMiddleware)
 
 	// Serve the client
 	workDir, _ := os.Getwd()
@@ -47,129 +40,6 @@ func main() {
 	http.ListenAndServe(":3333", r) // TODO: Make this port a config var
 }
 
-// This section contains helper functions and local middlewares
-// TODO : figure out a way to separate this out into a helper function file
-
-// ChiMongoMiddleware gives us our connection to the database
-func ChiMongoMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// setup the mgo connection
-		session, err := mgo.Dial("mongodb://localhost/") // TODO: make this a config var
-
-		if err != nil {
-			panic(err)
-		}
-
-		reqSession := session.Clone()
-		defer reqSession.Close()
-		db := reqSession.DB("chiact") // TODO: Make this a config var
-		ctx := context.WithValue(r.Context(), startup.DbKey, db)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-// ChiBasicMiddleware lets us process logins
-func ChiBasicMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		db := startup.GetDb(r.Context())
-		if db == nil {
-			log.Print("No database context")
-			http.Error(w, "Not authorized", 401)
-		}
-
-		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-
-		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-		if len(s) != 2 {
-			http.Error(w, "Not authorized", 401)
-			return
-		}
-
-		b, err := base64.StdEncoding.DecodeString(s[1])
-		if err != nil {
-			http.Error(w, err.Error(), 401)
-			return
-		}
-
-		pair := strings.SplitN(string(b), ":", 2)
-		if len(pair) != 2 {
-			http.Error(w, "Not authorized", 401)
-			return
-		}
-
-		//Find the user in the database
-		user, err := users.FindUserByEmail(pair[0], db)
-		if err != nil || user == nil {
-			log.Printf("User %+v not found.", pair[0])
-			http.Error(w, "Not authorized", 401)
-			return
-		}
-
-		//Check their password
-		err = user.CheckPassword(pair[1])
-		if err != nil {
-			log.Printf("Invalid password for User: %+v.", pair[0])
-			http.Error(w, "Not authorized", 401)
-			return
-		}
-
-		//clear password
-		user.Password = ""
-
-		//Set the logged in user in the context
-		ctx := context.WithValue(r.Context(), startup.UserKey, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-
-	})
-}
-
-// ChiJwtAuthMiddleware handles the JWT authentication strategy
-func ChiJwtAuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		db := startup.GetDb(r.Context())
-		if db == nil {
-			log.Print("No database context")
-			http.Error(w, "Not authorized", 401)
-		}
-
-		token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor, func(token *jwt.Token) (interface{}, error) {
-			return startup.JwtSecret, nil
-		})
-
-		if err != nil {
-			http.Error(w, "Invalid Token", 401)
-			return
-		}
-
-		if !token.Valid {
-			http.Error(w, "Invalid Token", 401)
-			return
-		}
-
-		// Check signing method to avoid vulnerabilities
-		if token.Method != jwt.SigningMethodHS256 {
-			http.Error(w, "Invalid Token", 401)
-			return
-		}
-		claims := token.Claims.(jwt.MapClaims)
-		//Find the user in the database
-		user, err := users.FindUserByID(claims["id"].(string), db)
-		if err != nil || user == nil {
-			log.Printf("User %s not found.", claims["id"].(string))
-			http.Error(w, "Not authorized", 401)
-			return
-		}
-
-		//clear password
-		user.Password = ""
-
-		//Set the logged in user in the context
-		ctx := context.WithValue(r.Context(), startup.UserKey, user)
-
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
 // ROUTING
 // This section defines the routes that will be served
 
@@ -178,7 +48,7 @@ func LoginRouter() chi.Router {
 	r := chi.NewRouter()
 
 	//r.Use(ChiMongoMiddleware)
-	r.Use(ChiBasicMiddleware)
+	r.Use(startup.BasicMiddleware)
 
 	r.Get("/", auth.SignInHandler)
 	return r
@@ -189,7 +59,7 @@ func APIRouter() chi.Router {
 	r := chi.NewRouter()
 
 	//r.Use(ChiMongoMiddleware)
-	r.Use(ChiJwtAuthMiddleware)
+	r.Use(startup.JwtAuthMiddleware)
 
 	r.Get("/me", auth.GetMeHandler)
 	r.Put("/me", auth.UpdateMeHandler)
